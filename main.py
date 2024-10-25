f3867335c1d22a6f4dbe6c62e1b9885a62ee6ce2
yahyathegreat <yahyamardan1234@gmail.com>
2024-10-25 19:50:40 +0500

    weather

--- main.py
+++ main.py
@@ -2,3 +2,9 @@ def well_wishes():
     print("hello")
     print("how are you")
 well_wishes()
+def weather_condition():
+    print('the weather is pleasant in:', spring)
+    print('the weather is the same in:', autumn)
+spring = "autumn"
+autumn = spring
+weather_condition()
