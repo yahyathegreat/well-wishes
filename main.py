ac2dbb632f61b018d794df4fabc073984488396e
yahyathegreat <yahyamardan1234@gmail.com>
2024-10-25 20:19:23 +0500

    rizz

--- main.py
+++ main.py
@@ -8,3 +8,29 @@ def weather_condition():
 spring = "autumn"
 autumn = spring
 weather_condition()
+def add (P, Q):
+    return P + Q
+def subtract (P, Q):
+    return P - Q
+def multiply (P, Q):
+    return P * Q
+def divide (P, Q):
+    return P / Q
+print("pkease select the operation")
+print("a. add")
+print("b. subtract")
+print("c. multiply")
+print("d. divide")
+choice = input("please enter your choice(a / b / c / d) ")
+num_1 = int (input("please enter the first number: "))
+num_2 = int(input("please enter the secind number: "))
+if choice == 'a':
+    print(num_1 "+",num_2, "=",add(num_1, num_2))
+elif choice == 'b':
+    print(num_1 "-",num_2, "=",subtract(num_1, num_2))
+elif choice == 'c':
+    print(num_1 "*",num_2, "=",multiply(num_1, num_2))
+ elif choice == 'd':
+    print(num_1 "/",num_2, "=",divide(num_1, num_2))
+ else:
+    print("this is an invalid input")
